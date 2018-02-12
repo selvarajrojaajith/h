1f49e816ad99b4d1121eed661b1f55f110bdd0b6
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-12 16:28:00 +0530

    integer.c

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int addNumbers(int n);
+
+int main()
+{
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Sum = %d",addNumbers(num));
+    return 0;
+}
+else{
+sum+add number;
+}
